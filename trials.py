e4a41c862d4ea06fd4109635a0320181f7558696
Tiela Black-Law <tiela.rose@gmail.com>
2023-04-11 21:18:04 -0700

    Complete all functions

--- trials.py
+++ trials.py
@@ -79,20 +79,73 @@ def censor_vowels(word):
 
 
 def snake_to_camel(string):
-    pass  # TODO: replace this line with your code
+    camelCase = []
 
+    for word in string.split('_'):
+        camelCase.append(f'{word[0].upper()}{word[1:]}')
+
+    return ''.join(camelCase)
+
+# print(snake_to_camel('hello_world'))
 
 def longest_word_length(words):
-    pass  # TODO: replace this line with your code
+    longest = len(words[0])
 
+    for word in words:
+        if longest < len(word):
+            longest = len(word)
+
+    return longest
+
+# print(longest_word_length(['jellyfish', 'zebra']))
 
 def truncate(string):
-    pass  # TODO: replace this line with your code
+    result = []
 
+    for letter in string:
+        if (len(result) == 0) or (letter != result[len(result)-1]):
+            result.append(letter)
+
+    return ''.join(result)
+
+# print(truncate('hi***!!!! wooow'))
 
 def has_balanced_parens(string):
-    pass  # TODO: replace this line with your code
+    parens = 0
 
+    for char in string:
+        if char == '(':
+            parens += 1
+        elif char == ')':
+            parens -= 1
+
+    return parens == 0
+
+# print(has_balanced_parens('(Oh no!)('))
 
 def compress(string):
-    pass  # TODO: replace this line with your code
+    compressed = []
+
+    curr_char = ''
+    char_count = 0
+
+    for char in string:
+        if char != curr_char:
+            compressed.append(curr_char)
+
+            if char_count > 1:
+                compressed.append(str(char_count))
+
+            curr_char = char
+            char_count = 0
+        
+        char_count += 1
+
+    compressed.append(curr_char)
+
+    if char_count > 1:
+        compressed.append(str(char_count))
+
+    return ''.join(compressed)
+
+print(compress('Hello, world! Cows go moooo...'))
